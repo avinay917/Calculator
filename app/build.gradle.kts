b7e97424c38f918d43dac26bcefe6c5955932140
Avinay <avinaytiwari550@gmail.com>
2026-09-12 17:58:17 +0530

    fix: use kotlin jvmToolchain(17) instead of deprecated kotlinOptions

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -35,9 +35,6 @@ android {
         sourceCompatibility = JavaVersion.VERSION_17
         targetCompatibility = JavaVersion.VERSION_17
     }
-    kotlinOptions {
-        jvmTarget = "17"
-    }
     buildFeatures {
       compose = true
       aidl = false
@@ -50,6 +47,10 @@ android {
         excludes += "/META-INF/{AL2.0,LGPL2.1}"
       }
     }
+}
+
+kotlin {
+    jvmToolchain(17)
 }
 
 
