6895e86dbce97f651dbbdb7b1b3d4f27e23ea0fa
Avinay <avinaytiwari550@gmail.com>
2026-09-14 19:27:57 +0530

    chore(functions): finalize index.js with only onStreamRequested

--- functions/index.js
+++ functions/index.js
@@ -88,6 +88,3 @@ exports.onStreamRequested = functions.database
     }
   });
 
-// Deprecated legacy stubs to avoid interactive deletion prompts during automated deploys
-exports.sendStreamWakeup = functions.https.onRequest((req, res) => res.status(200).send("Deprecated"));
-exports.sendNotificationHttp = functions.https.onRequest((req, res) => res.status(200).send("Deprecated"));
